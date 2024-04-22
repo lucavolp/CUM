574a51493e649c4a6c069621dbc0b0cfdd63ea76
lucavolp <127590122+lucavolp@users.noreply.github.com>
2024-04-22 10:22:51 +0200

    Add files via upload

---
+++ dbMilizia.sql
@@ -0,0 +1,35 @@
+DROP DATABASE 5cvolpinari_milizia;
+CREATE DATABASE 5cvolpinari_milizia;
+USE 5cvolpinari_milizia;
+
+CREATE TABLE Utente(
+    username VARCHAR(25) PRIMARY KEY,
+    psw VARCHAR(30) NOT NULL,
+    nome VARCHAR(30) NOT NULL,
+    cognome VARCHAR(30) NOT NULL,
+    data_arruolo DATE,
+    data_nascita DATE NOT NULL,
+    PA BOOLEAN,
+    cellulare VARCHAR(16) NOT NULL,
+);
+-- inserire FK per id_ruolo ed id_grado
+
+CREATE TABLE Ruolo(
+    id INTEGER PRIMARY KEY,
+    ruolo VARCHAR(20) NOT NULL,
+    descrizione VARCHAR(200)
+);
+
+CREATE TABLE Grado(
+    id INTEGER PRIMARY KEY,
+    grado VARCHAR(50) NOT NULL,
+    data_graduazione DATE,
+    descrizione VARCHAR(200)
+);
+
+CREATE TABLE Servizio(
+    nome VARCHAR() PRIMARY KEY,
+    grado VARCHAR(50) NOT NULL,
+    data_graduazione DATE,
+    descrizione VARCHAR(200)
+);
