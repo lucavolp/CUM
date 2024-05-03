5f61634175be31823fd4cf8c8181218d0ce18813
lucavolp <127590122+lucavolp@users.noreply.github.com>
2024-05-03 09:51:10 +0200

    Add files via upload

--- private/logout.php
+++ private/logout.php
@@ -1,5 +1,5 @@
 <?php
     session_start();
     session_destroy();
-    header('url: 10.25.0.15/~s_vlplcu05e21z130c/CUM/index.html');
+    header('header: 10.25.0.15/~s_vlplcu05e21z130c/CUM/index.html');
     exit;
