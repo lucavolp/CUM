e71d9de358fd172be1140801d1f9d1b41d96c858
lucavolp <s_vlplcu05e21z130c@itisurbino.it>
2024-05-07 00:17:54 +0800

    a

--- WSfestivita.php
+++ WSfestivita.php
@@ -35,17 +35,72 @@ $mesi = [
 $sql = "SELECT * FROM Festivita";
 $result = $conn->query($sql);
 $grouped_data = array();
+// $k=0;
+// if ($result->num_rows > 0) {
+//     while ($row = $result->fetch_assoc()) {
+//         if($k===0){
+//             $month = $row["num_mese"];
+//             $grouped_data[$month]=null;
+//             $k++;
+//         }
+//         if(is_null($grouped_data[$month])){
+//             // echo "<script>console.log('ciao');</script>";
+//             $month = $row["num_mese"];
+//             $lastM = $row["num_mese"];
+//             $grouped_data[$month] = array(
+//                 'mese' => $mesi[intval($month)],
+//                 'data' => $row['data'],
+//                 'nome' => $row['nome'],
+//                 'id' => $row['id']
+//             );
+//         }else{
+//             $month++;
+//             $grouped_data[$month] = array(
+//             'mese' => $mesi[intval($month-1)],
+//             'data' => $row['data'],
+//             'nome' => $row['nome'],
+//             'id' => $row['id']
+//             );
+//         }
+        
+//     }
+    
+// }
 
+$k = 0;
 if ($result->num_rows > 0) {
     while ($row = $result->fetch_assoc()) {
         $month = $row["num_mese"];
-        $grouped_data[$month] = array(
-            'mese' => $mesi[intval($month)],
-            'data' => $row['data'],
-            'nome' => $row['nome']
-        );
+        if ($k === 0) {
+            $grouped_data[$month] = null;
+            $k++;
+        }
+        if (is_null($grouped_data[$month])) {
+            $month = $row["num_mese"];
+            $lastM = $row["num_mese"];
+            $grouped_data[$month] = array(
+                'mese' => $mesi[intval($month)],
+                'data' => $row['data'],
+                'nome' => $row['nome'],
+                'id' => $row['id']
+            );
+        } else {
+            // Controlla se il mese corrente è già presente nell'array $grouped_data
+            while (array_key_exists($month, $grouped_data)) {
+                $month++;
+            }
+            
+            $grouped_data[$month] = array(
+                'mese' => "ciaoo",//$mesi[intval($month - 1)],
+                'data' => $row['data'],
+                'nome' => $row['nome'],
+                'id' => $row['id']
+            );
+        }
     }
 }
+
+
 
 $conn->close();
 
