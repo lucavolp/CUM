78658f14515b6f51c3c9856a2a2a45922b226904
lucavolp <s_vlplcu05e21z130c@itisurbino.it>
2024-05-13 23:31:49 +0200

    a

--- user/WSgradi.php
+++ user/WSgradi.php
@@ -2,12 +2,11 @@
 
     include("../assets/db/dbconn.php");
 
-
+/*
     $sql = "SELECT * FROM Grado";
     $result = $conn->query($sql);
     $grouped_data = array();
 
-    $k = 0;
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
             $gradi[] = $row;
@@ -26,3 +25,31 @@
     header('Content-Type: application/json');
     $json_formato = json_encode($grouped_data);
     echo $json_formato;
+
+
+
+    <?php
+    */
+    // Connessione al database
+
+    // Query per recuperare i dati dalla tabella 'grado'
+    $sql = "SELECT * FROM Grado";
+    $result = $conn->query($sql);
+
+    // Array per memorizzare i risultati
+    $gradi = array();
+
+    // Iterazione sui risultati della query e aggiunta dei dati all'array
+    if ($result->num_rows > 0) {
+        while($row = $result->fetch_assoc()) {
+            $gradi[] = $row["grado"];
+        }
+    } else {
+        $gradi[] = "Nessun risultato trovato";
+    }
+
+    // Converti l'array in formato JSON
+    echo json_encode($gradi);
+
+    // Chiusura della connessione al database
+    $conn->close();
