1453168e4729b0d77a5290297eac52e6ce5a14c9
lucavolp <s_vlplcu05e21z130c@itisurbino.it>
2024-05-18 16:48:43 +0200

    sabato 18 da casa

--- user/register.php
+++ user/register.php
@@ -4,7 +4,7 @@ header('Content-Type: application/json');
 
 function registerUser($usr, $pass, $nome, $cognome, $mail, $cell, $dataN, $dataA, $grado, $pa) {
 
-
+    
     include("../assets/db/dbconn.php");
 
     $crPsw=crypt($pass, PASSWORD_DEFAULT);          //d'ora in poi la password viene utilizzata criptata
@@ -13,21 +13,35 @@ function registerUser($usr, $pass, $nome, $cognome, $mail, $cell, $dataN, $dataA
 
     if ($result->num_rows <= 0) {
         if($usr!="admin"){
-            $sql = "INSERT INTO `Utente` ('usr', 'crPsw', 'nome', 'cognome', 'mail', 'cell', 'dataN', 'dataA', 'grado', 'pa') VALUES ($usr, $pass, $nome, $cognome, $mail, $cell, $dataN, $dataA, $grado, $pa);";
+            //if($pa==1)                    //NON FUNZIONA LA COSA DELLA PA
+                $pa=true;
+            $sql = "INSERT INTO `Utente` (`usr`, `pwd`, `nome`, `cognome`, `mail`, `cellulare`, `data_nascita`, `data_arruolo`, `id_grado`, `PA`, `id_ruolo`) VALUES ('$usr', '$crPsw', '$nome', '$cognome', '$mail', '$cell', $dataN, $dataA, $grado, $pa, 2);";
             $result = $conn->query($sql);
+            //echo $sql;
+            return array("success" => true, "message" => "Utente Creato", "risultato query" => $result);
         }else{
-            
-            return array("success" => false, "message" => "Username già in utilizzo");
+            return array("success" => false, "message" => "Username già in utilizzo", "result" => $result);
         }
-        return array("success" => true, "message" => "Utente Creato con successo");
     } else {
         return array("success" => false, "message" => "Username già in utilizzo");
     }
 }
 
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    include("../assets/db/dbconn.php");
     $json_data = file_get_contents("php://input");
     $data = json_decode($json_data, true);
+
+    $username = $data["username"];
+    $password = $data["password"];
+    $nome = $data["nome"];
+    $cognome = $data["cognome"];
+    $mail = $data["mail"];
+    $cellulare = $data["cellulare"];
+    $dataN = $data["dataN"];
+    $dataA = $data["dataA"];
+    $grado = $data["grado"];
+    $pa = $data["pa"];
 
     if ($data === null) {
         echo json_encode(array("success" => false, "message" => "Dati JSON non validi"));
@@ -41,16 +55,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         exit();
     }
 
-    $username = $data["username"];
-    $password = $data["password"];
-    $nome = $data["nome"];
-    $cognome = $data["cognome"];
-    $mail = $data["mail"];
-    $cellulare = $data["cellulare"];
-    $dataN = $data["dataN"];
-    $dataA = $data["dataA"];
-    $grado = $data["grado"];
-    $pa = $data["pa"];
+    
 
     $registrazione_result = registerUser($username, $password, $nome, $cognome, $mail, $cellulare, $dataN, $dataA, $grado, $pa);
 
