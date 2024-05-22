1d0ed3033f1d8ffc9b4a437a08a2a9b015ddb4ca
lucavolp <s_vlplcu05e21z130c@itisurbino.it>
2024-05-22 23:57:00 +0200

    get servizi da finire di rivedere, add funziona

--- private/ws/get_servizi.php
+++ private/ws/get_servizi.php
@@ -1,15 +1,28 @@
 <?php
 session_start();
 
+
+    //non so perche le assenze non le filtra bene
+
+
 if (!isset($_SESSION['username'])) {
     header("Location: ../accesso2.html");
     exit();
 }
-
+$logged_in_username = $_SESSION['username'];
 include("../../assets/db/dbconn.php");
 
+if($logged_in_username=='admin'){
+    $sql = "SELECT * FROM Servizio";
+        //echo $logged_in_username;       //DEBUG
+}else{
+    $sql = "SELECT * FROM Servizio WHERE 'usr_pers' = '".$logged_in_username."'";
+    //echo $logged_in_username;       //DEBUG
+}
+
 //$sql = "SELECT * FROM Assenza WHERE utente_usr='".$_SESSION['username']."'";
-$sql = "SELECT * FROM Servizio";
+
+//$sql = "SELECT * FROM Servizio";
 $result = $conn->query($sql);
 
 $assenze = array();
