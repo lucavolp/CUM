786eac12466002334b6589e7e793ec70af11400b
lucavolp <s_vlplcu05e21z130c@itisurbino.it>
2024-05-24 17:28:45 +0200

    casa2505

--- assets/db/dbconn.php
+++ assets/db/dbconn.php
@@ -4,12 +4,12 @@
     $dbpassword = "5cvolpinari";
     $dbname = "5cvolpinari_milizia";
 
-
+/*
     $servername = "192.168.1.152";
     $dbusername = "username";
     $dbpassword = "password";
     $dbname = "5cvolpinari_milizia";
-
+*/
 
     $conn = new mysqli($servername, $dbusername, $dbpassword, $dbname);
 
--- private/ws/get_servizi.php
+++ private/ws/get_servizi.php
@@ -16,22 +16,21 @@ if($logged_in_username=='admin'){
     $sql = "SELECT * FROM Servizio";
         //echo $logged_in_username;       //DEBUG
 }else{
-    $sql = "SELECT * FROM Servizio WHERE 'usr_pers' = '".$logged_in_username."'";
+    $sql = "SELECT * FROM Servizio WHERE usr_pers = '".$logged_in_username."'";
     //echo $logged_in_username;       //DEBUG
 }
 
-//$sql = "SELECT * FROM Assenza WHERE utente_usr='".$_SESSION['username']."'";
 
 //$sql = "SELECT * FROM Servizio";
 $result = $conn->query($sql);
 
-$assenze = array();
+$servizi = array();
 if ($result->num_rows > 0) {
     while($row = $result->fetch_assoc()) {
-        $assenze[] = $row;
+        $servizi[] = $row;
     }
 }
 
 $conn->close();
 header('Content-Type: application/json');
-echo json_encode($assenze);
+echo json_encode($servizi);
